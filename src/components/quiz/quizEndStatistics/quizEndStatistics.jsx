1a8c40db74e2e9ab08c7ffca78f222c0829333ad
polubis <polubis@wp.pl>
2018-10-21 10:27:57 +0200

    New things

--- src/components/quiz/quizEndStatistics/quizEndStatistics.jsx
+++ src/components/quiz/quizEndStatistics/quizEndStatistics.jsx
@@ -20,21 +20,48 @@ class QuizEndStatistics extends React.PureComponent{
         }).catch(() => this.setState({[stateItem]: false}));
     }
 
-    componentWillUnmount(){
-        console.log("Zegnaj");
-    }
-
     render(){
         const { isSavingQuizResult, isSavingQuizResultAgain } = this.state;
-        const { createResultErrors } = this.props;
+        const { createResultErrors, answers } = this.props;
         return (
             <div className="quiz-end-statistics">
                 {isSavingQuizResult ? <OperationPrompt positionClass="operation-prompt-tright"/> :
                     <ErrorHoc errors={createResultErrors} isRefresingRequest={isSavingQuizResultAgain} 
                         operation={() => this.saveResults("isSavingQuizResultAgain")}>
+                        <h1>Statystyki rozgrywki</h1>
+                        <div className="end-stats-container">
+                            <div className="stats-caffel">
+                                <div className="answers-ratio">
+                                    <span>4</span>
+                                    <span>/</span>
+                                    <span>6</span>
+                                </div>
+                            </div>
+                            <div className="stats-caffel">
+                                <div className="timers-ratio">
+                                    <div>
+                                        <i className="fa fa-clock-o"/>
+                                        <article>
+                                            <p>Twój czasowy rezultat jest bliski aktualnej średniej</p>
+                                            <p>Uzyskany wynik czasowy: <b>14.3 s</b></p>
+                                            <p>Aktualna średnia czasowa: <b>15.5 s</b></p>
+                                        </article>
+                                    </div>
+                                    <div className="time-line-container">
+                                        <div>
+                                            {answers.map((answer, index) => (
+                                                <span key={index} className={answer.isAnswerCorrect ? "greeny" : "reddy"}>
+                                                    <b>{answer.answerValue}</b>
+                                                    <b>{answer.timeForAnswer}</b>
+                                                </span>
+                                            ))}
+                                        </div>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                        <span>siema</span>
-                        
                     </ErrorHoc>
                 }
             </div>
